8be8a00705b033e5991b30db5a6ba9f453ff00be
rangga <ranggantwleng77@gmail.com>
2023-07-18 10:39:15 +0700

    next js new version

---
+++ app/about/[slug]/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const Page = (props) => {
+  console.log(props)
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
